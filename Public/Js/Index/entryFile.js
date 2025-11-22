0fcb477ae4e1445befe0500322738d825ec4a092
keshavsoft <nknnkumar@yahoo.com>
2025-11-22 11:36:57 +0530

    potato

--- Public/Js/Index/entryFile.js
+++ Public/Js/Index/entryFile.js
@@ -1,28 +1,11 @@
-const jFLocalTextToSpeech = ({ inText }) => {
-    // Create a new SpeechSynthesisUtterance object
-    const utterance = new SpeechSynthesisUtterance();
+import { StartFunc as StartFuncFormLoad } from "./FormLoad/entryFile.js";
 
-    // Set the text to be spoken
-    utterance.text = inText;
+const StartFunc = () => {
+    let jVarLocalFromAdmin = true;
 
-    // Optional: Configure speech parameters
-    utterance.volume = 1; // 0 to 1
-    utterance.rate = 1;   // 0.1 to 10
-    utterance.pitch = 1;  // 0 to 2
-
-    // Optional: Select a specific voice
-    // You would typically populate a dropdown with available voices first.
-    // Example:
-    // const voices = window.speechSynthesis.getVoices();
-    // utterance.voice = voices[0]; // Select the first available voice
-
-    // Use the SpeechSynthesis.speak() method to trigger speech synthesis
-    window.speechSynthesis.speak(utterance);
+    if (jVarLocalFromAdmin) {
+        StartFuncFormLoad();
+    };
 };
 
-const myButton = document.getElementById('PlayButtonId');
-
-// Add a click event listener
-myButton.addEventListener('click', () => {
-    jFLocalTextToSpeech({ inText: "Tomato rupees 60" });
-});
+StartFunc();
